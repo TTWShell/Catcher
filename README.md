de6516638a101a33abca675db876a9371c5d62c2
Legolas Bloom <zhanhsw@gmail.com>
2018-04-12 13:38:53 +0800

    filished influxdb conf

--- README.md
+++ README.md
@@ -2,6 +2,44 @@
 
 Telegraf (StatsD) + InfluxDB (storage) + Grafana (dashboard).
 
+一个单机部署的demo，仅供参考。可以自行集群化各组件。
+
+# Help
+
+查看命令帮助信息：
+
+    ./catcher.sh help
+
+程序启动 (./catcher.sh start) 后，可以使用一下命令测试：
+
+    echo "mycounter:10|c" | nc -C -w 1 -u 127.0.0.1 8125
+
+进入shell环境 (./catcher.sh shell)，使用influx命令，你将会在数据库看到一条记录：
+
+    Connected to http://localhost:8086 version 1.5.1
+    InfluxDB shell version: 1.5.1
+    > use telegraf;
+    Using database telegraf
+    > show measurements;
+    name: measurements
+    name
+    ----
+    cpu
+    disk
+    diskio
+    kernel
+    mem
+    mycounter
+    processes
+    swap
+    system
+    > select * from mycounter;
+    name: mycounter
+    time                host                    metric_type value
+    ----                ----                    ----------- -----
+    1523512020000000000 iZ2ze12cw2az4c7fwkbv8lZ counter     10
+    >
+
 # 参考
 
 1. [https://www.influxdata.com/blog/getting-started-with-sending-statsd-metrics-to-telegraf-influxdb/](https://www.influxdata.com/blog/getting-started-with-sending-statsd-metrics-to-telegraf-influxdb/)
@@ -9,3 +47,4 @@ Telegraf (StatsD) + InfluxDB (storage) + Grafana (dashboard).
 3. [http://blog.gezhiqiang.com/2017/01/25/statsd-summary/](http://blog.gezhiqiang.com/2017/01/25/statsd-summary/)
 4. [https://opsx.alibaba.com/mirror](https://opsx.alibaba.com/mirror)
 5. [https://portal.influxdata.com/downloads](https://portal.influxdata.com/downloads)
+6. [https://www.influxdata.com/blog/sensu-influxdb-storing-data-metrics-collection-checks/](https://www.influxdata.com/blog/sensu-influxdb-storing-data-metrics-collection-checks/)
--- catcher.sh
+++ catcher.sh
@@ -5,42 +5,75 @@ function echoo {
 }
 
 REGISTRY="registry-internal.cn-beijing.aliyuncs.com/ttwshell"
-IMG_NAME="catcher"
+PROJECT_NAME="catcher"
 IMG_TAG="latest"
-RUNTIME_IMAGE="${REGISTRY}/${IMG_NAME}:${IMG_TAG}-runtime"
+RUNTIME_IMAGE="${REGISTRY}/${PROJECT_NAME}:${IMG_TAG}-runtime"
 
 CUR_DIR=$(pwd)
 LOG_DIR="/data/catcher/logs"
+INFLUXDB_DIR="/data/catcher/influxdb"
 
 function create_data_volume {
-    docker inspect ${IMG_NAME}-data &> /dev/null
+    docker inspect ${PROJECT_NAME}-data &> /dev/null
     if [[ "$?" == "1" ]]; then
-        docker create --name ${IMG_NAME}-data \
+        docker create --name ${PROJECT_NAME}-data \
             -v ${CUR_DIR}:/root/catcher \
             -v ${LOG_DIR}:/root/catcher/logs \
+            -v ${INFLUXDB_DIR}:/root/catcher/influxdb/storage \
             hub.c.163.com/library/alpine:3.6 /bin/true
     fi
 }
 
+function check_exec_success {
+    if [[ "$1" != "0" ]]; then
+        echo "ERROR: $2 failed"
+        echo "$3"
+        exit -1
+    fi
+}
+
 function build-img {
-    command="docker build -t ${IMG_NAME}:${IMG_TAG} -f ./dockerfiles/Dockerfile ./dockerfiles $@"
+    command="docker build -t ${PROJECT_NAME}:${IMG_TAG} -f ./dockerfiles/Dockerfile ./dockerfiles $@"
     echoo "${command}" && eval ${command}
 }
 
 function release-img {
-    docker tag ${IMG_NAME}:${IMG_TAG} ${RUNTIME_IMAGE}
+    docker tag ${PROJECT_NAME}:${IMG_TAG} ${RUNTIME_IMAGE}
     docker push ${RUNTIME_IMAGE}
 }
 
 function shell {
     create_data_volume
     docker run --rm -it \
-        --volumes-from ${IMG_NAME}-data \
-        --net=host \
+        --volumes-from ${PROJECT_NAME}-data \
+        --net=container:${PROJECT_NAME} \
         ${RUNTIME_IMAGE} \
         /bin/bash
 }
 
+function start {
+    docker kill ${PROJECT_NAME} 2>/dev/null
+    docker rm -v ${PROJECT_NAME} 2>/dev/null
+
+    create_data_volume
+
+    docker run -d --name ${PROJECT_NAME} \
+        --volumes-from ${PROJECT_NAME}-data \
+        --restart=always \
+        --net=host \
+        --log-opt max-size=10m \
+        --log-opt max-file=9 \
+        ${RUNTIME_IMAGE} \
+        supervisord -c /root/catcher/supervisor/supervisord.conf
+
+    check_exec_success "$?" "start ${PROJECT_NAME} container"
+}
+
+function stop {
+    docker stop ${PROJECT_NAME} 2>/dev/null
+    docker rm -v ${PROJECT_NAME} 2>/dev/null
+    docker rm -v ${PROJECT_NAME} 2>/dev/null
+}
 
 # ---------------------------- Split
 function usage {
@@ -48,6 +81,8 @@ function usage {
     printf "\tbuild-img\tbuild docker image, you can use args of docker build.\n"
     printf "\trelease-img\trelease-img.\n"
     printf "\tshell\t\tshell env.\n"
+    printf "\tstart\t\tstart the project.\n"
+    printf "\tstop\t\tstop the project.\n"
 }
 
 Action=$1
@@ -57,5 +92,7 @@ case "${Action}" in
     build-img) build-img $@;;
     release-img) release-img;;
     shell) shell;;
+    start) start;;
+    stop) stop;;
     *) usage && exit 1 ;;
 esac
